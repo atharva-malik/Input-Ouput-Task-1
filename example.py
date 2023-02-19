bb41e35beba2a88e70ad86816349ef717a793aa3
Atharva <63628518+atharva-malik@users.noreply.github.com>
2023-02-19 11:34:00 +1100

    Fixed the example.py adding the extension code

--- example.py
+++ example.py
@@ -2,15 +2,20 @@ import random  # Imports the random module used to generate random numbers
 
 numGuesses = 0  # Keeps track of the number of guesses the user has made
 randNum = random.randint(1, 100)  # Generates a random number between 1 and 100
-while True:  # Runs the code forever
-    userGuess = int(input("Guess a number between 1 and 100 (type STOP to stop): "))  # Asks the user to guess a number and turns it into a number
-    numGuesses += 1  # Adds 1 to the number of guesses the user has made
-    if int(userGuess) > randNum:
-        print("Lower!")  # Tells the user that their guess was too high
-    elif int(userGuess) < randNum:
-        print("Higher!")  # Tells the user that their guess was too low
-    elif int(userGuess) == randNum:
-        print("You got it! It took you " + str(numGuesses) + " guesses.")  # Tells the user that they got it and how many guesses it took
-        break
+while True:
+    while True:  # Runs the code forever
+        userGuess = int(input("Guess a number between 1 and 100: "))  # Asks the user to guess a number and turns it into a number
+        numGuesses += 1  # Adds 1 to the number of guesses the user has made
+        if int(userGuess) > randNum:
+            print("Lower!")  # Tells the user that their guess was too high
+        elif int(userGuess) < randNum:
+            print("Higher!")  # Tells the user that their guess was too low
+        elif int(userGuess) == randNum:
+            print("You got it! It took you " + str(numGuesses) + " guesses.")  # Tells the user that they got it and how many guesses it took
+            break
+        else:
+            print("Wrong input!")  # Tells the user that the input was wrong
+    if input("Type STOP to stop: ") == "STOP":
+        break  # Stops the program
     else:
-        print("Wrong input!")  # Tells the user that the input was wrong
+        continue  # Restarts the program
