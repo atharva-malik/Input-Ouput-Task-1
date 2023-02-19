e622c362050c7436cbc6a5acb2c0fe7f981f7229
Atharva <63628518+atharva-malik@users.noreply.github.com>
2023-02-19 11:52:59 +1100

    Fixed the example.py adding the code to catch wrong input

--- example.py
+++ example.py
@@ -4,7 +4,10 @@ numGuesses = 0  # Keeps track of the number of guesses the user has made
 randNum = random.randint(1, 100)  # Generates a random number between 1 and 100
 while True:
     while True:  # Runs the code forever
-        userGuess = int(input("Guess a number between 1 and 100: "))  # Asks the user to guess a number and turns it into a number
+        try:
+            userGuess = int(input("Guess a number between 1 and 100: "))  # Asks the user to guess a number and turns it into a number
+        except ValueError:
+            print("Please enter a number.")  # Tells the user that they need to enter a number
         numGuesses += 1  # Adds 1 to the number of guesses the user has made
         if int(userGuess) > randNum:
             print("Lower!")  # Tells the user that their guess was too high
@@ -13,8 +16,6 @@ while True:
         elif int(userGuess) == randNum:
             print("You got it! It took you " + str(numGuesses) + " guesses.")  # Tells the user that they got it and how many guesses it took
             break
-        else:
-            print("Wrong input!")  # Tells the user that the input was wrong
     if input("Type STOP to stop: ") == "STOP":
         break  # Stops the program
     else:
